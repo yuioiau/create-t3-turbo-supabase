e4cc9390b7b4582d52f8d667e45447813ae4eb3a
default <hello@techwithanirudh.com>
2024-06-20 14:24:43 +0000

    style: Update CreatePostForm to use background color for input fields

--- apps/nextjs/src/app/dashboard/_components/posts.tsx
+++ apps/nextjs/src/app/dashboard/_components/posts.tsx
@@ -58,7 +58,7 @@ export function CreatePostForm() {
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input {...field} placeholder="Title" className="bg-white" />
+                <Input {...field} placeholder="Title" className="bg-background" />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -70,7 +70,7 @@ export function CreatePostForm() {
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input {...field} placeholder="Content"  className="bg-white" />
+                <Input {...field} placeholder="Content"  className="bg-background" />
               </FormControl>
               <FormMessage />
             </FormItem>
